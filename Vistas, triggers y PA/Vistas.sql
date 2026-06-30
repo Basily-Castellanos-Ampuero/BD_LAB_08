e501f1a6ef4ab074bafbbf7dc0153307cb53453e
Dai49 <calarico@unsa.edu.pe>
2026-06-29 19:46:04 -0500

    Vista 3

--- Vistas, triggers y PA/Vistas.sql
+++ Vistas, triggers y PA/Vistas.sql
@@ -21,3 +21,39 @@ SELECT
 FROM G1M_CLIENTES c
 JOIN GZZ_TIP_CLI  tc ON c.CliTipCod = tc.TipCliCod;
 SELECT * FROM VI_CLIENTES_TIPO;
+
+CREATE OR REPLACE VIEW VI_CLIENTES_TIPO AS
+SELECT 
+    c.CliCod      AS 'Código',
+    c.CliNom      AS 'Cliente',
+    tc.TipCliDes  AS 'Tipo',
+    c.CliFecIng   AS 'Fecha Ingreso',
+    c.CliFecCes   AS 'Fecha Cese'
+FROM G1M_CLIENTES c
+JOIN GZZ_TIP_CLI  tc ON c.CliTipCod = tc.TipCliCod;
+SELECT * FROM VI_CLIENTES_TIPO;
+
+CREATE OR REPLACE VIEW VI_PROYECTOS_COMPLETO AS
+SELECT 
+    cab.ProCliCod   AS 'Cód. Cliente',
+    cli.CliNom      AS 'Cliente',
+    tc.TipCliDes    AS 'Tipo Cliente',
+    tp.TipProDes    AS 'Tipo Proyecto',
+    cab.ProSec      AS 'Secuencia',
+    cab.ProFecCon   AS 'F. Contrato',
+    cab.ProFecIni   AS 'F. Inicio',
+    cab.ProFecEnt   AS 'F. Entrega',
+    cab.ProFecCer   AS 'F. Cierre',
+    ep.EstProDes    AS 'Estado',
+    cab.ProMonPre   AS 'Monto Presup.',
+    cab.ProMonRea   AS 'Monto Real',
+    cab.ProCosPre   AS 'Costo Presup.',
+    cab.ProCosRea   AS 'Costo Real',
+    cab.ProUtiPre   AS 'Utilidad Presup.',
+    cab.ProUtiRea   AS 'Utilidad Real'
+FROM G1T_PRO_CAB  cab
+JOIN G1M_CLIENTES cli ON cab.ProCliCod = cli.CliCod
+JOIN GZZ_TIP_CLI  tc  ON cli.CliTipCod = tc.TipCliCod
+JOIN GZZ_TIP_PRO  tp  ON cab.ProTipCod = tp.TipProCod
+JOIN GZZ_EST_PRO  ep  ON cab.ProEstCod = ep.EstProCod;
+SELECT * FROM VI_PROYECTOS_COMPLETO;
